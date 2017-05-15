b7706f3465e8b96ffeed1d9324ba7c5079c5affb
HP <HP@User-HP>
2017-05-16 01:13:26 +0300

    test commit

--- JitTest/src/Test.java
+++ JitTest/src/Test.java
@@ -1,8 +1,7 @@
-
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world"); 
-		system.out
+		System.out.println("hello world");
+		System.out.println(" test commit 16 - 5 - 2017");
 	}
 
 }
